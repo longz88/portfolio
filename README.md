94c459060d479e3d24da204c7d57d8a78f642074
long <chuthanhlong2k2@gmail.com>
2023-11-29 23:38:20 +0700

    second

---
+++ README.md
@@ -0,0 +1,19 @@
+<!--tạo git -->
+
+git init
+
+git add .
+
+git commit -m "first commit"
+
+git branch -M main
+
+git remote add origin https://github.com/longz88/ha.git
+
+git push -u origin main
+
+<!-- đẩy git -->
+
+git add .
+
+git push -u origin main
---
+++ index.js
@@ -0,0 +1,30 @@
+const tabLinks = document.querySelectorAll(".tab-links");
+const tabContents = document.querySelectorAll(".tab-contents");
+const tabNavs = document.querySelectorAll(".nav-bar li a");
+
+const openTab = (tabName) => {
+  for (tabLink of tabLinks) {
+    tabLink.classList.remove("active-link");
+  }
+
+  for (tabContent of tabContents) {
+    tabContent.classList.remove("active-tab");
+  }
+
+  for (tabNav of tabNavs) {
+    tabNav.classList.remove("active-nav");
+  }
+
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabName).classList.add("active-tab");
+};
+
+tabNavs.forEach((tab) => {
+  tab.addEventListener("click", function () {
+    document
+      .querySelector(".nav-bar li a.active-nav")
+      .classList.remove("active-nav");
+
+    this.classList.add("active-nav");
+  });
+});
